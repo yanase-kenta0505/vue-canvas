d755728cff8d208ae7b307de0d786ec229d1abce
yanasekenta <ynsknt0505@gmail.com>
2022-09-21 12:53:27 +0900

    canvasでimageを描画する

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,37 @@
-<script setup>
-import { ref } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue';
+const canvas = ref<HTMLCanvasElement>();
 
-defineProps({
-  msg: String
-})
+const draw = () => {
+  if (!canvas.value) return
+  const ctx: CanvasRenderingContext2D | null = canvas.value.getContext('2d')
+  if (!ctx) return
+  const image = ctx.createImageData(150, 150)
+  // 青色
+  for (let i = 0; i < image.data.length; i++) {
+    image.data[i * 4] = 46
+    image.data[i * 4 + 1] = 135
+    image.data[i * 4 + 2] = 243
+    image.data[i * 4 + 3] = 255
+  }
 
-const count = ref(0)
+  ctx.putImageData(image, 0, 0)
+}
+
+
+
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h1>canvas lesson</h1>
+  <canvas width="300" height="300" ref='canvas' />
+  <button @click="draw" >draw</button>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+canvas {
+  border: 3px solid red;
 }
 </style>
+
