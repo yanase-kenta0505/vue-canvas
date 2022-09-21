a138bdd5cdcca3a4d62b2d488ac1e2efb67c2665
yanasekenta <ynsknt0505@gmail.com>
2022-09-21 16:32:10 +0900

    colorを切り替える

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,22 +6,25 @@ let timeOutId = ref()
 let x = ref(0)
 let y = ref(0)
 
+
 const draw = () => {
   if (!canvas.value) return
   const ctx: CanvasRenderingContext2D | null = canvas.value.getContext('2d')
   if (!ctx) return
   const image = ctx.createImageData(10, 10)
   // 青色にする
-  const makeBlue = () => {
-    for (let i = 0; i < image.data.length; i++) {
-      image.data[i * 4] = 46
-      image.data[i * 4 + 1] = 135
-      image.data[i * 4 + 2] = 243
-      image.data[i * 4 + 3] = 255
-    }
+
+  for (let i = 0; i < image.data.length; i++) {
+
+    image.data[i * 4] = 46
+    image.data[i * 4 + 1] = 135
+    image.data[i * 4 + 2] = 243
+    image.data[i * 4 + 3] = 255
   }
+
   const makeBrown = () => {
     for (let i = 0; i < image.data.length; i++) {
+      console.log('yaa')
       image.data[i * 4] = 243
       image.data[i * 4 + 1] = 108
       image.data[i * 4 + 2] = 24
@@ -29,28 +32,36 @@ const draw = () => {
     }
   }
 
-
-  timeOutId.value = setInterval(function drawImge() {
-    makeBlue()
+  timeOutId.value = setInterval(function test() {
     ctx.putImageData(image, x.value, y.value * 10)
     y.value++
-    if (y.value * 10 === canvas.value?.height) {
+    if (!canvas.value) return
+    if (y.value * 10 === canvas.value.height || y.value * 10 === 0) {
+      // 青色に変更
+      for (let i = 0; i < image.data.length; i++) {
+        image.data[i * 4] = 46
+        image.data[i * 4 + 1] = 135
+        image.data[i * 4 + 2] = 243
+        image.data[i * 4 + 3] = 255
+      }
       y.value = 0
       x.value += 10
+    } else if (y.value * 10 === canvas.value.height / 2) {
+      // 茶色に変更
+      for (let i = 0; i < image.data.length; i++) {
+        image.data[i * 4] = 243
+        image.data[i * 4 + 1] = 108
+        image.data[i * 4 + 2] = 24
+        image.data[i * 4 + 3] = 255
+      }
     }
-
-  }, 100)
+  }, 10)
 }
+
 
 const stop = () => clearInterval(timeOutId.value)
 
-
-
-
-
-
 </script>
-
 <template>
   <h1>canvas lesson</h1>
   <canvas width="300" height="300" ref='canvas' />
